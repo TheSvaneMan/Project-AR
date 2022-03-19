86d57a04cdf26effc5f926ef93efb3a6362eed85
Penda Djunior Kanime Svane <thesvaneman@gmail.com>
2022-03-19 15:05:14 +0100

    v.1.1.3 : ☕ UnityLib reattach
    -> rebuilt manifest
    -> Changed icon to purple icon
    
    ** Bug Found : Splash screen not updating

--- android/app/src/main/java/io/svane/app/MainActivity.java
+++ android/app/src/main/java/io/svane/app/MainActivity.java
@@ -1,5 +1,14 @@
 package io.svane.app;
+import android.os.Bundle; // required for onCreate parameter
+
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        registerPlugin(EchoPlugin.class);
+        registerPlugin(UnityIonicPlugin.class);
+    }
+}
---
+++ android/app/src/main/java/io/svane/app/UnityIonicPlugin.java
@@ -0,0 +1,44 @@
+package io.svane.app;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.PluginMethod;
+
+@CapacitorPlugin(name = "UnityIonicPlugin")
+public class UnityIonicPlugin extends Plugin{
+    @PluginMethod()
+    public void startUnity(PluginCall call) {
+        String value = call.getString("value");
+        JSObject ret = new JSObject();
+        ret.put("value", value);
+        call.resolve(ret);
+        Intent intent = new Intent(getContext(), MainUnityActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        getActivity().startActivity(intent);
+        // JSObject ret = new JSObject();
+        // ret.put("value", value);
+        // call.resolve(ret);
+    }
+
+    @PluginMethod()
+    public void changeObjectName(PluginCall call){
+        String value = call.getString("value");
+        // New object name
+        MainUnityActivity.getInstance().changeObjectName(value);
+        JSObject ret = new JSObject();
+        ret.put("value", value);
+        call.resolve(ret);
+        // JSObject ret = new JSObject();
+        // ret.put("value", value);
+        // call.resolve(ret);
+    }
+}
