a070e119ad2ae84964daa13d0111f696c2ab93ff
Penda Svane <thesvaneman@gmail.com>
2022-03-20 12:38:20 +0100

    v.1.1.7
    -> Small changes to the renderMap component
    -> Added dummy data, for Marker usage
    -> Moved some code into function based declaration and usage.
    -> Did some styling on the markers
    
    ** Map Feature WIP

---
+++ src/components/Map/MapMarkerData.tsx
@@ -0,0 +1,260 @@
+// ------------------- Marker Data ----------------------- //
+const geojson = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Sunday Post",
+                'image': "https://i.imgur.com/Mgi7Onk.png",
+                'uid': "zBBryzCdA5Zwj1b3ye5SStzw6a72"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.150134576613084, 56.11740601203417]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/W6E5u6d.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.151409529326088, 56.11582124666077]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/o10N7po.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.20247078085663, 56.57834944076201]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/5fIQqnr.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.20647078085640, 56.23534944076241]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/Ilr13lF.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.21247078086630, 56.15934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/3muQt6T.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.21247078086630, 56.20934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/7SjEPAN.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.01247078086630, 56.34934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/JiGcBpJ.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.02247078086630, 56.38934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/9pMch5c.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.12247078086630, 56.78934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/d0R63vd.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [10.22247078086630, 56.88934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/q6T8idG.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [11.22247078086630, 54.28934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/I02glM0.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [11.21247078086630, 55.28934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/3Jm93FS.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [11.24247078086230, 40.28934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/NiMmLek.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [11.44247078086630, 40.88934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/iXxqT1Y.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [19.54247078086630, 42.88934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/nYwDZfH.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [11.54247078086630, 38.88934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/54BhVUk.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [12.54247078086630, 39.82934944076291]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Unique AR Post',
+                'iconSize': [60, 60],
+                'title': "Real deal",
+                'image': "https://i.imgur.com/Fa0HM82.jpeg",
+                'uid': "CXqXyhClm2Mox3R3JqgCUsSdyCo1"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [13.54247078086630, 36.22934944076291]
+            }
+        }
+    ]
+};
+
+export default geojson;
--- src/components/Map/RenderMap.tsx
+++ src/components/Map/RenderMap.tsx
@@ -4,8 +4,11 @@ import { Geolocation } from '@capacitor/geolocation';
 import './RenderMap.css';
 import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import { useEffect, useState } from 'react';
-import { IonButton, IonContent, IonIcon, IonItem, IonLabel, IonSearchbar, IonToggle } from '@ionic/react';
+import { IonButton, IonContent, IonIcon, IonImg, IonItem, IonLabel, IonSearchbar, IonToggle } from '@ionic/react';
 import { sunny } from 'ionicons/icons';
+import { get, onValue } from '@firebase/database';
+import { postsRef, usersRef } from '../../firebase-config';
+import geojson from './MapMarkerData';
 
 const RenderMap: React.FC<any> = () => {
     // Default Geo location is Windhoek, Namibia
@@ -13,25 +16,74 @@ const RenderMap: React.FC<any> = () => {
     const [currentLatitude, setCurrentLatitude] = useState<any>(-22.55941);
     const [currentLongitude, setCurrentLongitude] = useState<any>(17.08323);
     const [searchText, setSearchText] = useState('');
+    let map: any;
+    // Get Post Data to use on Map
+    // Many improvements can be done here so data loading is done once on a service, stored and shared between pages and components
+    // Too many API calls
+    const [posts, setPosts] = useState<any>([]);
 
     const alertUser = () => {
         alert("Updated Preferences");
-      }
+    }
 
-    useEffect(() => {
+    function createMap() {
         // TO MAKE THE MAP APPEAR YOU MUST
         // ADD YOUR ACCESS TOKEN FROM
         // https://account.mapbox.com
         // IMPORTANT: Mask this auth key for MapBox access to geo-db
         mapboxgl.accessToken = 'pk.eyJ1IjoidGhlc3ZhbmVtYW4iLCJhIjoiY2wwczJ0bndhMDkyczNrcWtwemZrbGRpdiJ9.SvwyHaNaN0wH7RMl4XujfA';
         // Generate the map area before first render of the component
-        const map = new mapboxgl.Map({
+        map = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/thesvaneman/cl0sggbkc000b15po2u9ovbbc', // style URL
             center: [currentLongitude, currentLatitude], // starting position [lng, lat]
             zoom: 15 // starting zoom
         });
+    }
 
+    function addMarkers() {
+        // Add markers to the map.
+        for (const marker of geojson.features) {
+            // Default Custom Marker  (Purple Point)
+            const defMarker = document.createElement('div');
+            defMarker.className = 'defCustomMarker';
+            // Create Post Card (Options Menu)
+            const postCard = document.createElement('div');
+            postCard.className = 'customMapMarker';
+            // Create a DOM element for each marker.
+            const el = document.createElement('div');
+            const width = marker.properties.iconSize[0];
+            const height = marker.properties.iconSize[1];
+            el.style.backgroundImage = `url(${marker.properties.image})`;
+            el.style.width = `100%`;
+            el.style.height = `100%`;
+            el.style.backgroundSize = '100%';
+            el.style.backgroundRepeat = `no-repeat`;
+            postCard.appendChild(el);
+            defMarker.appendChild(postCard);
+            defMarker.addEventListener('click', () => {
+                togglePosts(postCard);
+            });
+
+            // Deconstruct array to provide long & lat 
+            let lon: number = marker.geometry.coordinates[0];
+            let lat: number = marker.geometry.coordinates[1];
+            const lonlat = { lon, lat };
+            // Add markers to the map.
+            new mapboxgl.Marker(defMarker)
+                .setLngLat(lonlat)
+                .addTo(map);
+        }
+    }
+
+    function togglePosts(postCard: any) {
+        postCard.classList.toggle("customMapMarkerOpen");
+    }
+    useEffect(() => {
+        // Creates the map
+        createMap();
+        // Add the markerts based on the post data
+        addMarkers();
     });
 
     // Geo Location 
@@ -54,24 +106,25 @@ const RenderMap: React.FC<any> = () => {
         console.log('API Geo response', postLocation);
         console.log('Current latitude:', latitude);
         console.log('Current longitude:', longitude);
-        
+
         setCurrentLatitude(latitude);
         setCurrentLongitude(longitude);
         setLocation(postLocation);
     }
+
     return (
         <>
             <div className='mapSettings' id='settingsToggle'>
-      <IonItem className='mapSwitch' lines="none">
-        <IonIcon slot="start" icon={sunny} />
-        <IonLabel>Show Nearby Art</IonLabel>
-        <IonToggle slot="end" name="Show Art" onIonChange={alertUser} />
-      </IonItem>
+                <IonItem className='mapSwitch' lines="none">
+                    <IonIcon slot="start" icon={sunny} />
+                    <IonLabel>Show Nearby Art</IonLabel>
+                    <IonToggle slot="end" name="Show Art" onIonChange={alertUser} />
+                </IonItem>
                 <IonButton className='mapButton nftHunt'>NFT Hunt</IonButton>
                 <IonButton className='mapButton nftHunt' onClick={setCurrentPosition}>
-                Display Current Location
-            </IonButton>
-      <IonSearchbar className='mapSearch' value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
+                    Display Current Location
+                </IonButton>
+                <IonSearchbar className='mapSearch' value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
             </div>
             <div id="map">
 
